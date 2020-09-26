1f7192bf0d75e8993cf1eb4c77a48bff76f80c7b
eneri-jr <eneri.jr@gmail.com>
2020-09-26 10:33:00 -0300

    Criacao Parte E (Alteracao Curso)

--- src/br/com/digitalHouse/Curso.kt
+++ src/br/com/digitalHouse/Curso.kt
@@ -1,6 +1,12 @@
 package br.com.digitalHouse
 
-class Curso(var codCurso: Int, var nome: String) {
+class Curso(var codCurso: Int,
+            var nome: String,
+            var profTitu: ProfessorTitular,
+            var profAdj: ProfessorAdjunto,
+            var maxAlun: Int,
+            var listaAlun: List<Aluno>
+            ) {
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
