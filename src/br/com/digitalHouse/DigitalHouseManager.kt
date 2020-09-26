47770cedc2656d7eb6f6b5a40b2c7c4b62ba95a3
eneri-jr <eneri.jr@gmail.com>
2020-09-26 10:52:38 -0300

    Criacao Parte G (Classe DigitalHouseManager)

---
+++ src/br/com/digitalHouse/DigitalHouseManager.kt
@@ -0,0 +1,6 @@
+package br.com.digitalHouse
+
+class DigitalHouseManager(var listAlun: List<Aluno>,
+                          var listProf: List<Professor>,
+                          var listCurso: List<Curso>,
+                          var listMatric: List<Matricula>)
