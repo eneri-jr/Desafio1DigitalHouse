791a3b6984b0b1063306facf2bc001721447b13f
eneri-jr <eneri.jr@gmail.com>
2020-09-25 22:01:11 -0300

    Criacao Parte C (Classe Professor), (Alteracao Aluno), (Interface Pessoa)

--- src/br/com/digitalHouse/Curso.kt
+++ src/br/com/digitalHouse/Curso.kt
@@ -1,16 +1,12 @@
 package br.com.digitalHouse
 
-class Curso (
-        var codCurso: Int,
-        var nome : String
-)
-{
+class Curso(var codCurso: Int, var nome: String) {
 
     override fun equals(other: Any?): Boolean {
-        if(this === other) return true
-        if(other !is Curso) return false
+        if (this === other) return true
+        if (other !is Curso) return false
 
-        if(codCurso != other.codCurso) return false
+        if (codCurso != other.codCurso) return false
 
         return true
     }
---
+++ src/br/com/digitalHouse/Pessoa.kt
@@ -0,0 +1,7 @@
+package br.com.digitalHouse
+
+interface Pessoa {
+
+    var nome: String
+    var sobrenome: String
+}
